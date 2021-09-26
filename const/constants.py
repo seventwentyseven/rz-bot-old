548a09f06b249c32e9827b0acf7f98910db972f7
def750 <def75023@gmail.com>
2021-09-26 21:30:43 +0200

    Updated profile
    [+] Added display of user discord if linked in database
    [*] Changed version 0.3.6

--- const/constants.py
+++ const/constants.py
@@ -1,7 +1,7 @@
 import cmyui
 
 #Don't touch it. I'm changing it on updates, it's also (will be) used for updater
-version = cmyui.Version(0, 3, 5)
+version = cmyui.Version(0, 3, 6)
 
 #Don't touch this too, it's mysql connection stuff
 db = cmyui.AsyncSQLPool()
